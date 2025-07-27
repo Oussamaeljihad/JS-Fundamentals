c6f3332688de2c626c442bc1cad44c16c717ac5a
Oussamaeljihad <mr.oussamaeljihad@gmail.com>
2025-07-27 15:17:19 +0100

    Add script to print lines using array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let result = '';
+
+for (let i = 0; i < messages.length; i++) {
+  result += messages[i];
+  if (i < messages.length - 1) {
+    result += '\n';
+  }
+}
+
+console.log(result);
